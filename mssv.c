cfbdc3cebecc87b59a7b0156edd4574a8777eeee
btdun2002 <54074236+btdun2002@users.noreply.github.com>
2021-05-08 21:43:37 +0700

    Add files via upload

---
+++ mssv.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+main ()
+	{int n,a[8];
+	int i;
+	scanf("%d",&n);
+	for (i=7;i>=0;i--)
+	{a[i]=n/pow(10,i);
+	n=n-a[i]*pow(10,i);}
+	for (i=7;i>=0;i--)
+	{	switch (a[i]) {
+		case 0:
+		{printf("khong");
+			break;}
+		case 1:
+		{printf("mot");
+			break;}
+		case 2:
+		{printf("hai");
+			break;}
+		case 3:
+		{printf("ba");
+			break;}
+		case 4:
+		{printf("bon");
+			break;}
+		case 5:
+		{printf("nam");
+			break;}
+		case 6:
+		{printf("sau");
+			break;}
+		case 7:
+		{printf("bay");
+			break;}
+		case 8:
+		{printf("tam");
+			break;}
+		case 9:
+		{printf("chin");
+			break;}
+		}
+		printf(" ");
+	}
+	}
